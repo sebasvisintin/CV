a725da4adf22bb1740d347cdb6b49c1e9431c970
Sebastian <31627716+sebasvisintin@users.noreply.github.com>
2018-02-02 16:15:33 -0300

    Add files via upload

---
+++ webscraping/appNoticias.js
@@ -0,0 +1,47 @@
+/* 1 */
+{
+    "_id" : ObjectId("5a74b090bed8091010d77e8a"),
+    "titulo" : "(Unpatched) Adobe Flash Player Zero-Day Exploit Spotted in the Wild",
+    "imagen" : "0.jpg",
+    "__v" : 0
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("5a74b090bed8091010d77e8b"),
+    "titulo" : "Meltdown/Specter-based Malware Coming Soon to Devices Near You, Are You Ready?",
+    "imagen" : "1.jpg",
+    "__v" : 0
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("5a74b090bed8091010d77e8c"),
+    "titulo" : "Get the Ultimate 2018 Hacker Bundle &#x2013; Pay What You Want\n ",
+    "imagen" : "2.jpg",
+    "__v" : 0
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("5a74b090bed8091010d77e8d"),
+    "titulo" : "Cryptocurrency Mining Malware Infected Over Half-Million PCs Using NSA Exploit",
+    "imagen" : "3.jpg",
+    "__v" : 0
+}
+
+/* 5 */
+{
+    "_id" : ObjectId("5a74b090bed8091010d77e8f"),
+    "titulo" : "Update Your Firefox Browser to Fix a Critical Remotely Exploitable Flaw",
+    "imagen" : "5.jpg",
+    "__v" : 0
+}
+
+/* 6 */
+{
+    "_id" : ObjectId("5a74b090bed8091010d77e8e"),
+    "titulo" : "Critical Oracle Micros POS Flaw Affects Over 300,000 Payment Systems",
+    "imagen" : "4.jpg",
+    "__v" : 0
+}
